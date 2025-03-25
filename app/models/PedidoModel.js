f19c930ac7aec83a430f6507b0c6aa57381c9012
YiromanOmg <yiromanml@outlook.com>
2025-03-25 17:57:28 -0600

    Update PedidoModel.js

--- app/models/PedidoModel.js
+++ app/models/PedidoModel.js
@@ -10,7 +10,27 @@ const PedidoSchema = new mongoose.Schema({
         cantidad: { type: Number, required: true }, // Cantidad solicitada
         precioUnitario: { type: Number, required: true }, // Precio del producto en el momento de la compra
         subtotal: { type: Number, required: true }, // Calculado como cantidad * precioUnitario
-        imagenDiseno: { type: String } // URL de la imagen del diseño
+        imagenDiseno: { type: String },// URL de la imagen del diseño
+
+
+        // CALCULO DE COSTOS
+        tipo: { type: String, enum: ['Imprenta', '3D'], required: true },
+
+        costoProducto: { type: Number },
+        costoImpresion: { type: Number },
+
+        gramosUsados: { type: Number },
+        costoFilamento: { type: Number },
+        horasProduccion: { type: Number },
+        costoPorHora: { type: Number },
+        desgasteMaquina: { type: Number },
+        costoExtra: { type: Number },
+
+        precioCosto: { type: Number },         // Calculado por tipo
+        totalCosto: { type: Number },
+        comision: { type: Number },
+        totalComision: { type: Number },
+        utilidadReal: { type: Number }
     }],
     total: { type: Number, required: true }, // Suma de subtotales de productos
     fechaCreacion: { type: Date, default: Date.now }, // Fecha automática de creación
@@ -26,7 +46,19 @@ const PedidoSchema = new mongoose.Schema({
         required: true
     }, // Método de pago usado
     cuentaDestino: { type: mongoose.Schema.Types.ObjectId, ref: 'Cuenta' }, // Cuenta donde se recibió el pago
-    notas: { type: String } // Notas adicionales sobre el pedido
+    notas: { type: String }, // Notas adicionales sobre el pedido
+
+
+
+    // DATOS DE COSTOS
+    totalCosto: { type: Number },       // Opcional
+    totalComision: { type: Number },    // Opcional
+    utilidadReal: { type: Number },     // Opcional
+    tipoGeneral: {
+        type: String,
+        enum: ['Imprenta', '3D', 'Mixto'],
+    },
+
 }, { timestamps: true }); // timestamps agrega automáticamente `createdAt` y `updatedAt`
 
 module.exports = PedidoModel = mongoose.model('pedidos', PedidoSchema);
