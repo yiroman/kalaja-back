e76430d3bcdbf20dc45fe5080585553e906128ae
YiromanOmg <yiromanml@outlook.com>
2025-03-31 17:27:08 -0600

    Update app.js

--- app/app.js
+++ app/app.js
@@ -57,6 +57,7 @@ const dominiosPermitidos = () => {
 		return [
 			'http://localhost:4200',
 			'http://localhost:4201',
+			'kalaja-front-git-main-yiromans-projects.vercel.app',
 			'https://kalaja-front-4yqv.vercel.app',
 		]
 	}
@@ -64,6 +65,7 @@ const dominiosPermitidos = () => {
 		return [
 			'http://localhost:4200',
 			'https://kalaja-front-4yqv.vercel.app',
+			'kalaja-front-git-main-yiromans-projects.vercel.app',
 			'http://localhost:4201'
 		]
 	}
